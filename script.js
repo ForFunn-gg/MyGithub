8ea8cec9df4e495fa9b6cd69db2ecf401a6a20ad
ForFunn-gg <dmaronjake@gmail.com>
2026-01-19 18:36:24 +0800

    Day 3

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const navbar = document.getElementById('navbar')
+function openSidebar(){
+    navbar.classList.add('show')
+
+}
+
+function closedSidebar(){
+    navbar.classList.remove('show')
+
+}
